2ebbd1541ac3a08e35d6b6dc2133a1d815959666
jahp8719@gmail.com <jahp8719@gmail.com>
2023-09-18 07:47:49 -0400

    feact: adding commit intial commit

--- backend_label_ws/label_ws/label_ws/wsgi.py
+++ backend_label_ws/label_ws/label_ws/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'label_ws.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "label_ws.settings")
 application = get_wsgi_application()
+app = application
